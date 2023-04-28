7a3158a01f58a70303f5a672e435c8ec676e6152
Michael Z <michaelzwierzanski@gmail.com>
2023-04-28 15:27:31 +0100

    add font awesome

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <h1>Footer</h1>
+      <FontAwesomeIcon icon={faCoffee} />
+    </div>
+  );
 };
 
 export default Footer;
