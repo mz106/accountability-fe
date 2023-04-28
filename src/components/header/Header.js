487b24b5e669c7de832d8c3492542be02358f349
Michael Z <michaelzwierzanski@gmail.com>
2023-04-28 17:38:34 +0100

    add basic footer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,19 +5,21 @@ import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
-    <div className="header-wrapper">
-      <div className="header-title-wrapper">
-        <div className="header-logo-wrapper">
-          <FontAwesomeIcon icon={faCoffee} className="header-logo" />
+    <div className="header-wrapper-outer">
+      <div className="header-wrapper-inner">
+        <div className="header-title-wrapper">
+          <div className="header-logo-wrapper">
+            <FontAwesomeIcon icon={faCoffee} className="header-logo" />
+          </div>
+          <div>
+            <p>App Name</p>
+          </div>
         </div>
-        <div>
-          <p>App Name</p>
-        </div>
-      </div>
 
-      <div className="header-login-sign-menu">
-        <div>Sign-up</div>
-        <div>Login</div>
+        <div className="header-login-sign-menu">
+          <div>Sign-up</div>
+          <div>Login</div>
+        </div>
       </div>
     </div>
   );
