32223eca2d10aec6f108c14cfc1d2c2b60b61e07
Michael Z <michaelzwierzanski@gmail.com>
2023-04-28 14:34:26 +0100

    add basic layout header footer

---
+++ src/components/generalLayout/Layout.js
@@ -0,0 +1,14 @@
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
